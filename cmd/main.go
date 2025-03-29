a76b8daeee09cb31912c5390eb221cd634fe625e
Alina_Zubko <ali1304sha@gmail.com>
2025-03-30 01:09:32 +0300

    добавила бд postres

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"log"
-
+	"github.com/spf13/viper"
 	"github.com/AlinaZbk/ToDo-App"
 	"github.com/AlinaZbk/ToDo-App/pkg/handler"
 	"github.com/AlinaZbk/ToDo-App/pkg/repository"
@@ -10,13 +10,23 @@ import (
 )
 
 func main(){
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	
 	srv := new(todo.Server) //созадем http-сервер
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil { //запускаем сервер
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil { //запускаем сервер
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs") //путь к конфигу
+	viper.SetConfigName("config") //имя кофигурационного файла
+	return viper.ReadInConfig() //читаем конфиг
 }
