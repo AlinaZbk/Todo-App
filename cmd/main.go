193264d8d8aa541e1ab4d6d191118f30e6ac61fb
Alina_Zubko <ali1304sha@gmail.com>
2025-03-29 19:58:04 +0300

    зависимости

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+
 	"github.com/AlinaZbk/ToDo-App"
 	"github.com/AlinaZbk/ToDo-App/pkg/handler"
+	"github.com/AlinaZbk/ToDo-App/pkg/repository"
+	"github.com/AlinaZbk/ToDo-App/pkg/service"
 )
 
 func main(){
-	handlers := new(handler.Handler) //создаем обработчик http-запросов
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	
 	srv := new(todo.Server) //созадем http-сервер
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil { //запускаем сервер
 		log.Fatalf("error occured while running http server: %s", err.Error())
