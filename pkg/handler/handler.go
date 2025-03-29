fc4e1584d8d4577cac4b724c078fd93cb3ed87bc
Alina_Zubko <ali1304sha@gmail.com>
2025-03-29 16:53:08 +0300

    добавила функции хендлера

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -12,27 +12,27 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	auth := router.Group("/auth")
 	{
-		auth.POST("/sign-up") //обработчик регистрации
-		auth.POST("/sign-in") //обработчик входа
+		auth.POST("/sign-up", h.signUp) //обработчик регистрации
+		auth.POST("/sign-in", h.signIn) //обработчик входа
 	}
 
 	api := router.Group("/api")
 	{ 
 		lists := api.Group("/lists") //будет использована для работы со списком
 		{
-			lists.POST("/") //создание нового списка
-			lists.GET("/") //получение всех списков
-			lists.GET("/:id") //получение списка по id
-			lists.PUT("/:id") //редактирование списка по id
-			lists.DELETE("/:id") //удаление списка по id
+			lists.POST("/", h.createList) //создание нового списка
+			lists.GET("/", h.getAllLists) //получение всех списков
+			lists.GET("/:id", h.getListById) //получение списка по id
+			lists.PUT("/:id", h.updateList) //редактирование списка по id
+			lists.DELETE("/:id", h.deleteList) //удаление списка по id
 
 			items := lists.Group(":id/items") //группа для задач списка
 			{
-				items.POST("/") //создание новой задачи в списке
-				items.GET("/") //получение всех задач списка
-				items.GET("/:item_id") //получение задачи по id
-				items.PUT("/:items_id") //редактирование задачи по id
-				items.DELETE("/:item_id") //удаление задачи по id
+				items.POST("/", h.createItem) //создание новой задачи в списке
+				items.GET("/", h.getAllItems) //получение всех задач списка
+				items.GET("/:item_id", h.getItemById) //получение задачи по id
+				items.PUT("/:items_id", h.updateItem) //редактирование задачи по id
+				items.DELETE("/:item_id", h.deleteItem) //удаление задачи по id
 			}
 		}
 	}
